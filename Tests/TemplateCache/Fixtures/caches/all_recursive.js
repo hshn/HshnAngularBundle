6dc2ff496a3add401beb68749109895c6f25552f
Shota Hoshino <lga0503@gmail.com>
2014-07-13 19:03:01 +0900

    Update some test fixtures

--- Tests/TemplateCache/Fixtures/caches/all_recursive.js
+++ Tests/TemplateCache/Fixtures/caches/all_recursive.js
@@ -7,22 +7,16 @@ angular.module('all')
     '    bar c\n' +
     '</div>\n' +
     '');
-  }])
-  .run(['$templateCache', function ($templateCache) {
     $templateCache.put('bar/d.html',
     '<div class="foo" id=\'bar\'>\n' +
     '    bar d\n' +
     '</div>\n' +
     '');
-  }])
-  .run(['$templateCache', function ($templateCache) {
     $templateCache.put('foo/a.html',
     '<div class="foo" id=\'bar\'>\n' +
     '    foo a\n' +
     '</div>\n' +
     '');
-  }])
-  .run(['$templateCache', function ($templateCache) {
     $templateCache.put('foo/b.html',
     '<div class="foo" id=\'bar\'>\n' +
     '    foo b\n' +
