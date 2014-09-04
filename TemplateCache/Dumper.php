bb953d113637097dff502b40e46b681d765974e6
Shota Hoshino <lga0503@gmail.com>
2014-09-04 19:25:08 +0900

    [TemplateCache] Add dumper

---
+++ TemplateCache/Dumper.php
@@ -0,0 +1,60 @@
+<?php
+
+
+namespace Hshn\AngularBundle\TemplateCache;
+use Symfony\Component\Filesystem\Filesystem;
+
+
+/**
+ * @author Shota Hoshino <lga0503@gmail.com>
+ */
+class Dumper
+{
+    /**
+     * @var TemplateCacheManager
+     */
+    private $manager;
+
+    /**
+     * @var TemplateFinder
+     */
+    private $finder;
+
+    /**
+     * @var Compiler
+     */
+    private $compiler;
+
+    /**
+     * @var string
+     */
+    private $dumpPath;
+
+    /**
+     * @param TemplateCacheManager $manager
+     * @param TemplateFinder       $finder
+     * @param Compiler             $compiler
+     * @param string               $dumpPath
+     */
+    public function __construct(TemplateCacheManager $manager, TemplateFinder $finder, Compiler $compiler, $dumpPath)
+    {
+        $this->manager = $manager;
+        $this->finder = $finder;
+        $this->compiler = $compiler;
+        $this->dumpPath = $dumpPath;
+    }
+
+    public function dump()
+    {
+        $content = '';
+
+        foreach ($this->manager->getModules() as $config) {
+            if ($templates = $this->finder->find($config)) {
+                $content .= $this->compiler->compile($templates, $config->getModuleName());
+            }
+        }
+
+        $fs = new Filesystem();
+        $fs->dumpFile($this->dumpPath, $content);
+    }
+}
