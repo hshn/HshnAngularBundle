1a2310ecce91984ad18243f974ee874cb58af23c
Shota Hoshino <lga0503@gmail.com>
2014-09-06 14:07:47 +0900

    Add tests for service definition

---
+++ Tests/Functional/ServiceContainerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace Hshn\AngularBundle\Tests\Functional;
+
+
+/**
+ * @author Shota Hoshino <lga0503@gmail.com>
+ */
+class ServiceContainerTest extends WebTestCase
+{
+    /**
+     * @test
+     */
+    public function testServicesHaveRegistered()
+    {
+        static::bootKernel();
+
+        $container = static::$kernel->getContainer();
+
+        $ids = array(
+            'hshn_angular.template_cache.template_finder',
+            'hshn_angular.template_cache.manager',
+            'hshn_angular.template_cache.compiler',
+            'hshn_angular.template_cache.dumper',
+            'hshn_angular.command.dump_template_cache',
+        );
+
+        foreach ($ids as $id) {
+            $this->assertTrue($container->has($id), $id);
+        }
+    }
+}
